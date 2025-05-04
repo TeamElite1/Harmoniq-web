6d6b35245f79b9ea9867aafd1418c42936c5146c
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2025-05-04 17:27:44 +0100

    chore: setup index page and general setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div> this is the home page of harmoniq application</div>
+    <div> this is the home page of harmoniq application.</div>
   )
 }
 
